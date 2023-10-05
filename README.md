4cbf122c376a5410a2694496dd980de52393292f
Jimmy <zinuhe@outlook.com>
2023-10-04 21:59:40 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # javascript-tetris
-Tetris
+
+## Create
+
+```shell
+npm create vite@latest
+```
+
+## Install dependencies
+
+```shell
+pnpm install
+```
+
+## Instal linter
+
+```shell
+pnpm install standard -D
+```
+
+## Config linter
+
+[Documentation](https://eslint.org/docs/latest/use/configure/)
+
+On **`package.json`** add at the end
+
+```javascript
+"eslintConfig": {
+    "extends":[
+        "standard"
+    ]
+  }
+```
+
+## Canvas
+
+Add a canvas to draw on it
+
+# To Know
+
+- const context = canvas.getContext('2d');
+
+# To RUN
+
+```shell
+pnpm run dev
+```
---
+++ tetris/main.js
@@ -0,0 +1,106 @@
+import './style.css'
+
+// 1 - Initialize canvas
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+const BLOCK_SIZE = 20;
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+// Tetris board dimensions
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale (BLOCK_SIZE, BLOCK_SIZE)
+
+// 3 - Board
+//  '0' - represent a void
+//  '1' - represent a block
+const board  = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+]
+
+// 4 - Pieces
+const piece = {
+    position: { x: 5, y: 5 }, // initial position on the board
+    shape: [
+        [1, 1], // square
+        [1, 1]
+    ]
+}
+
+// 2 - Game Loop
+function update() {
+    draw()
+    window.requestAnimationFrame(update) // Loop - alternative: The setInterval() method
+}
+
+function draw() {
+    context.fillStyle = "#000" // Background - black
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    // Draw the board
+    board.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value === 1) {
+                context.fillStyle = 'yellow'
+                context.fillRect(x, y, 1, 1)
+            }
+        })
+    })
+
+    // Draw the pieces
+    piece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value) {
+                context.fillStyle = "red"
+                context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+            }
+        })
+    })
+}
+
+// Move the piece
+document.addEventListener('keydown', event => {
+    if(event.key === 'ArrowLeft')  piece.position.x--
+    if(event.key === 'ArrowRight') piece.position.x++
+    if(event.key === 'ArrowDown') piece.position.y++
+})
+
+update()
+
+
+
+
+
+
